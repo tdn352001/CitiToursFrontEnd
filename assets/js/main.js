c6985761af54f6018e5194260937c9aeff976ba4
tdn352001 <tdn352001@gmail.com>
2021-09-18 21:18:57 +0700

    slider animation, chưa có floating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,73 @@
-const slider = document.querySelector('.slider');
-const sliderProgress = document.querySelector('.slider__progress');
-const slide1 = document.querySelector('.slide__item-first');
-const slide2 = document.querySelector('.slide__item-second');
-const btnSlideLeft = document.querySelector('.btn__change-slide-left svg');
-const btnSlideRight = document.querySelector('.btn__change-slide-right svg');
-let isSlider1 = true;
+// const slider = document.querySelector('.slider');
+// const sliderProgress = document.querySelector('.slider__progress');
+// const slide1 = document.querySelector('.slide__item-first');
+// const slide2 = document.querySelector('.slide__item-second');
+// const btnSlideLeft = document.querySelector('.btn__change-slide-left svg');
+// const btnSlideRight = document.querySelector('.btn__change-slide-right svg');
+// const sliderElementsHasAnimation = document.querySelectorAll('.slider__element-animation')
+// const btnBuyTour = document.querySelector('.slider_tour-btn');
+// let isSlider1 = true;
+// let currentIndex = 0;
+// let SetTimeout;
+// const changeSlide = function () {
+//     // if (SetTimeout){
+//     //     clearTimeout(SetTimeout);
+//     //     SetTimeout = null;
+//     // }
+//     currentIndex = 0;
+//     HideAllElementAnimation();
+//     ResetAllElementAnimation(currentIndex);
+//     isSlider1 = !isSlider1;
+//     if (isSlider1) {
+//         slide1.style.display = 'none';
+//         slide2.style.display = 'block';
+//         btnBuyTour.innerText = 'BUY THE TOUR'
+//     } else {
+//         slide1.style.display = 'block';
+//         slide2.style.display = 'none';
+//         btnBuyTour.innerText = 'BUY A LICENSE'
 
-const changeSlide = function () {
-    resetAnimation();
-    isSlider1 = !isSlider1;
-    if (isSlider1) {
-        slide1.style.display = 'none';
-        slide2.style.display = 'block';
-    } else {
-        slide1.style.display = 'block';
-        slide2.style.display = 'none';
-    }
-    
-}
+//     }
+// }
 
-const resetAnimation = () => {
-    sliderProgress.style.animation = 'none';
-    sliderProgress.offsetHeight; 
-    sliderProgress.style.animation = null;
-}
+// const HideAllElementAnimation = () => {
+//     resetAnimation(sliderProgress);
+//     for (element of sliderElementsHasAnimation){
+//         element.classList.add('display_none');
+//     }
+// }
 
-let autoChangeSlide = setInterval(changeSlide, 5000);
+// const ResetAllElementAnimation= (currentIndex) => {
+//     SetTimeout = setTimeout(() => {
+//         if (currentIndex < sliderElementsHasAnimation.length){
+//             let element = sliderElementsHasAnimation[currentIndex];
+//             element.classList.remove('display_none');
+//             resetAnimation(element)
+//             currentIndex++;
+//             ResetAllElementAnimation(currentIndex);
+//         }
+//     }, 300)
+// }
 
-const ClickChangeSlide = () => {
-    clearInterval(autoChangeSlide)
-    autoChangeSlide = null;
-    changeSlide()
-    autoChangeSlide = setInterval(changeSlide, 5000);
-}
+// const resetAnimation = (element) => {
+//     element.style.animation = 'none';
+//     element.offsetHeight;
+//     element.style.animation = null;
+// }
 
-btnSlideLeft.onclick = ClickChangeSlide
-btnSlideRight.onclick = ClickChangeSlide
+// HideAllElementAnimation();
+// ResetAllElementAnimation(currentIndex);
+// let autoChangeSlide = setInterval(changeSlide, 5000);
+
+// const ClickChangeSlide = () => {
+//     clearInterval(autoChangeSlide)
+//     autoChangeSlide = null;
+//     changeSlide()
+//     autoChangeSlide = setInterval(changeSlide, 5000);
+// }
+
+// btnSlideLeft.onclick = ClickChangeSlide
+// btnSlideRight.onclick = ClickChangeSlide
 
 
 
